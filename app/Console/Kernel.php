b3f0fa4f0a61dc4669909e9af767cf0715cdb0c9
shahram peyvandi <yasfuny@gmail.com>
2020-08-03 18:30:32 +0430

    432

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Schedules\CheckUserExpireScheduler;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,6 +25,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+
+        $schedule->call(new CheckUserExpireScheduler)->dailyAt('09:00');
+
         // $schedule->command('inspire')->hourly();
     }
 
